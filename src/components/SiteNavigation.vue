8af4646f2a826aa124d531daab15e3c411be3ff5
Robert Ling <ling.robert@hotmail.com>
2023-12-15 19:25:16 +0800

    finish basemodel & siteNavigation a bit of homeview

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,24 +1,66 @@
 <template>
-  <header class="sticky top-0 bg-weather-primary shadow-lg">
+  <header class="sticky top-0 shadow-lg bg-weather-primary">
     <nav
-      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+      class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row"
     >
       <RouterLink :to="{ name: 'home' }">
-        <div class="flex items-center gap-3 flex-1">
-          <i class="fa-regular fa-sun text-2xl"></i>
+        <div class="flex items-center flex-1 gap-3">
+          <i class="text-2xl fa-regular fa-sun"></i>
           <span class="text-2xl">The Local Weather</span>
         </div>
       </RouterLink>
       <!-- Add Icons using String format -->
-      
-      <div class="flex gap-3 flex-1 justify-end">
-        <i class="fa-solid fa-circle-info text-2xl hover:text-weather-secondary duration-150 cursor-pointer"></i>  
-        <i class="fa-solid fa-plus text-2xl hover:text-weather-secondary duration-150 cursor-pointer"></i>  
+
+      <div class="flex justify-end flex-1 gap-3">
+        <i
+          @click="toggleModel"
+          class="text-2xl duration-150 cursor-pointer fa-solid fa-circle-info hover:text-weather-secondary"
+        ></i>
+        <i
+          class="text-2xl duration-150 cursor-pointer fa-solid fa-plus hover:text-weather-secondary"
+        ></i>
       </div>
+      <BaseModal :modalActive="modelActive" @handleModel="toggleModel">
+        <div class="text-black">
+          <h1 class="mb-1 text-2xl">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it works:</h2>
+          <ol class="mb-4 list-decimal list-inside">
+            <li>
+              Search for your city by entering the name into the search bar.
+            </li>
+            <li>
+              Select a city within the results, this will take you to the
+              current weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. This
+              will save the city to view at a later time on the home page.
+            </li>
+          </ol>
+
+          <h2 class="text-2xl">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be am option to
+            delete the city.
+          </p>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
 import { RouterLink } from "vue-router";
+import { defineProps, ref } from "vue";
+import BaseModal from "./BaseModel.vue";
+
+const modelActive = ref(null);
+const toggleModel = () => {
+  modelActive.value = !modelActive.value;
+};
 </script>
